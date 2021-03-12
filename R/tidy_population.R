a97459bdb21521d6ba06c11de0bbc9a5dccbf208
YitaoLiu1996 <liuyitao1996@gmail.com>
2021-03-12 00:20:02 -0500

    Add the explanation of the variables and example,

--- R/tidy_population.R
+++ R/tidy_population.R
@@ -2,18 +2,26 @@
 #'
 #' Get population level tidy data
 #'
-#' @param population_from
-#' @param population_where
-#' @param treatment_var
-#' @param treatment_order
-#' @param stratum_var
-#' @param baseline_var
+#' @param population_from The input population level data table. For example, "subject level information" from the table "adsl".
+#' @param population_where Tells the function which rows of the table should be select, according to the value of certain column. For example, select subset where "ITTFL=='Y'.
+#' @param treatment_var The function will create a new column called "treatment" based on the column of this parameter. For example, the "treatment" column will be equal to "TRT01A".
+#' @param treatment_order Tells the function which rows of the table should be select, only if the values in "treatment" is in this parameter. It also provide the label names of the treatments after turning them into factors. For example, include rows which treatment = "Placebo" and treatment = "Xanomeline High Dose", and change the factor labels into "Placebo" and "MK9999".
+#' @param stratum_var The function will create a new column called "stratum" based on this parameter.
+#' @param baseline_var Tells the function which column of the table to be selected as the final output.
 #'
-#' @return a table of population information
+#' @return an output table of population information.
 #'
 #' @export
 #'
 #' @examples
+#' pop <- tidy_population(population_from  = adsl %>% rename(TRTA = TRT01A),
+#'  population_where = "ITTFL=='Y'",
+#'  treatment_var    = "TRTA",
+#'  treatment_order  = c("MK9999" = "Xanomeline High Dose", "Placebo" = "Placebo"),
+#'  stratum_var      = NULL,
+#'  baseline_var     = NULL)
+#'
+#'
 tidy_population <- function(population_from,
                             population_where = NULL,
                             treatment_var = "TRT01A",
