0f874b8e386fa9630f3715d94a2d90c08e1c973c
Melody J <melody@nbp-27-251.nbp.ruw.rutgers.edu>
2021-03-26 10:18:45 -0400

    add documentation for sparkline_drawaxis

--- R/sparkline_errorbar.R
+++ R/sparkline_errorbar.R
@@ -1,38 +1,43 @@
 #' sparkline_errorbar
-#' 
-#' @examples 
-#' 
-#' est <- c(0,1,2)
-#' lower <- c(-1, -2, -1)
-#' upper <- c(3, 5, 5)
-#' color <- c("red", "blue", "gold")
-#' sparkline_errorbar(est, lower, upper, color)
-#' 
+#' @param est A vector of
+#' @param lower
+#' @param upper
+#' @param color
+#' @param height
+#'
+#' @examples
+#'
+est <- c(0,1,2)
+lower <- c(-1, -2, -1)
+upper <- c(3, 5, 5)
+color <- c("red", "blue", "gold")
+sparkline_errorbar(est, lower, upper, color)
+#'
 #' @export
-sparkline_errorbar <- function(est, 
-                               lower, 
-                               upper, 
-                               color = NULL, 
+sparkline_errorbar <- function(est,
+                               lower,
+                               upper,
+                               color = NULL,
                                height = 30,
-                               xlim = NULL, 
-                               vline = NULL, 
-                               margin = list(l = 0, r = 0, b = 0, t = 0, pad = 0), 
+                               xlim = NULL,
+                               vline = NULL,
+                               margin = list(l = 0, r = 0, b = 0, t = 0, pad = 0),
                                ...){
-  
-  db <- data.frame(est = est, 
-                   lower = lower, 
-                   upper = upper, 
+
+  db <- data.frame(est = est,
+                   lower = lower,
+                   upper = upper,
                    mid = (lower + upper) / 2)
-  
+
   db$sd <- db$upper - db$mid
-  
+
   db$est1   <- formatC(db$est, digits = 2, format = "f")
   db$lower1 <- formatC(db$lower, digits = 2, format = "f")
   db$upper1 <- formatC(db$upper, digits = 2, format = "f")
-  
+
   hover_text <- with(db, paste0(est1, " (",lower1, ", ", upper1, ")") )
-  
-  p <- plot_ly(data = db, 
+
+  p <- plot_ly(data = db,
                x = ~ est,
                y = ~ 1:nrow(db),
                color = color,
@@ -42,16 +47,16 @@ sparkline_errorbar <- function(est,
                type = 'scatter',
                mode = 'markers',
                height = height)
-  
-  p <- p %>% add_trace(x = ~ mid, 
-                       alpha = 0, 
+
+  p <- p %>% add_trace(x = ~ mid,
+                       alpha = 0,
                        text = hover_text,
                        hoverinfo = 'text',
                        error_x =  ~list(array = sd, color = color))
-  
-  p %>% sparkline_layout(xlim = xlim, 
-                         vline = vline, 
-                         margin = margin, 
+
+  p %>% sparkline_layout(xlim = xlim,
+                         vline = vline,
+                         margin = margin,
                          ...)
-  
+
 }
