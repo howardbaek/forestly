6157ca176d71b8039bf9255ecbb51388fb40eeb0
kyle9960 <mykzhao@gmail.com>
2021-04-01 14:22:15 +0800

    aaa

--- R/sparkline_point.R
+++ R/sparkline_point.R
@@ -1,24 +1,28 @@
 #' sparkline_point
-#' 
-#' @examples 
-#' 
+#'
+#' This funtion is
+#'
+#' @param
+#'
+#' @examples
+#'
 #' x <- 1:3
 #' y <- 1:3
 #' color <- c("red", "blue", "gold")
 #' sparkline_point(x = x, y = y, color = color, height = 30)
-#' 
+#'
 #' @export
 sparkline_point <- function(
                             x,
                             y,
-                            color = NULL, 
+                            color = NULL,
                             height = 30,
-                            hover_text = NULL, 
-                            xlim = NULL, 
-                            vline = NULL, 
-                            margin = list(l = 0, r = 0, b = 0, t = 0, pad = 0), 
+                            hover_text = NULL,
+                            xlim = NULL,
+                            vline = NULL,
+                            margin = list(l = 0, r = 0, b = 0, t = 0, pad = 0),
                             ...){
-  
+
   p <- plot_ly(
                x = x,
                y = y,
@@ -29,10 +33,10 @@ sparkline_point <- function(
                type = 'scatter',
                mode = 'markers',
                height = height)
-  
-  p %>% sparkline_layout(xlim = xlim, 
-                         vline = vline, 
-                         margin = margin, 
+
+  p %>% sparkline_layout(xlim = xlim,
+                         vline = vline,
+                         margin = margin,
                          ...)
-  
+
 }
