6901c484871c24247cdb51521a99bf83c82742c0
YitaoLiu1996 <liuyitao1996@gmail.com>
2021-04-02 01:11:57 -0400

    .

--- R/tidy_population.R
+++ R/tidy_population.R
@@ -1,15 +1,17 @@
 #' tidy_population
 #'
-#' This function is used to prepare the observation datasets for the use in function tidy_ae_table().
-#' The function filter the 'adsl' dataset, and define a 'treatment' in the dataset.
+#' This function is used to prepare the observation datasets for the use in function `tidy_ae_table()`.
+#' The function filter the `adsl` dataset, and define proper treatment order in the dataset.
 #'
 #' @param population_from A data frame to obtain population level variables.
 #'                        Typically an 'adsl' dataset.
 #'                        It is the source of variables mentioned in
-#'                        'population_where', 'treatment_var', 'stratum_var' and 'covariate_var'.
+#'                        `population_where`, `treatment_var`, `stratum_var` and `covariate_var`.
 #' @param population_where A character string to define the criteria to select analysis population.
 #' @param treatment_var  A character string to define the variable of new column called "treatment".
-#' @param treatment_order A vector of character strings that tells the function which rows of the table should be select, only if the values in "treatment" is in this vector.
+#' @param treatment_order A vector of character strings that tells the
+#'                        function which rows of the table should be select,
+#'                        only if the values in "treatment" is in this vector.
 #'                        It also provide the label names of the treatments after turning them into factors.
 #' @param stratum_var A character string to define the variable of baseline stratum in 'population_from'.
 #'                    Only one 'stratum_var' is allowed.
