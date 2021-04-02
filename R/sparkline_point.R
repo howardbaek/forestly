0d6141e249fa59c9d40072bc31f12cc7d58918e7
kyle9960 <mykzhao@gmail.com>
2021-04-02 21:38:41 +0800

    color

--- R/sparkline_point.R
+++ R/sparkline_point.R
@@ -1,19 +1,24 @@
 #' sparkline_point
 #'
-#' This function is drawing the scatter plot
+#' This function is drawing the scatters on the sparkline plot
 #'
-#' @param
 #'
+#' @param x a data frame corresponding location of scatters on x-axis
+#' @param y a data frame corresponding location of scatters on y-axis
+#' @param color A character string define the color of different scatters.
+#' @param height A number define the height of the vertical line
+#' @param hover_text A character string describe the information of each scatters
+#' @param xlim A data frame define the range of x-axis on the plot
+#' @param vline A data frame indicates where to draw the vertical lines on the plot
+#' @param margin A data define the size of the plot
 #' @examples
-#'
-#' x <- 1:3
-#' y <- 1:3
-#' color <- c("red", "blue", "gold")
-#' sparkline_point(x = x, y = y, color = color, height = 30)
-#'
+#' library(plotly)
+#' x <- 1:5
+#' y <- 1:5
+#' color <- c("red", "blue", "gold", "grey", "black")
+#' sparkline_point(x = x, y = y, color = color, height = 500, vline =1.5,
+#'                hover_text = c('a','b','c','d','e'))
 #' @export
-#'
-library(plotly)
 sparkline_point <- function(
                             x,
                             y,
@@ -42,9 +47,3 @@ sparkline_point <- function(
                          ...)
 
 }
-
-x <- 1:5
-y <- 1:5
-color <- c("red", "blue", "gold","grey","green")
-sparkline_point(x = x, y = y, color = color, height = 50)
-
