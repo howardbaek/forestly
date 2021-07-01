01f7d2fe433313d3133c0221e9e4438181e413c3
Yujie Zhao <yujie.zhao@merck.com>
2021-07-01 13:16:51 -0400

    Add checkbox to forest plot:
    - Add three additional function in the R folder:
      - tidy_ae_table1 is to generate dataframe with only ONE ae structure, e.x., AESER.
      - tidy_ae_table2 is to generate dataframe with multiply ae structure, e.x., c(AESER, AESEV,...)
      - tidy_population1 is different from tidy_population in line #55
    
    - Add two vignettes
      - one uses the `sparkline_point_js`
      - one uses the original cell plot function.

---
+++ R/tidy_ae_table2.R
@@ -0,0 +1,95 @@
+#' tidy_ae_table2
+#'
+#' Define analysis data with multiple AE structure, 
+#' i.e., we have check box:
+#' []AESEV []AEREL []AEOUT
+#' 
+#' This function is used to obtain AE information ready for visualization
+#'
+#'
+#' @param population_from A data frame to obtain population level variables.Typically an 'adsl' dataset.
+#' @param observation_from A data frame to obtain observation level variables.
+#' @param population_where A character string to define the criteria to select analysis population.
+#' @param observation_where A character string to define the criteria to select analysis observation.
+#' @param treatment_var A character string to define the variable of new column called "treatment"
+#' @param treatment_order A character vector to define the treatment display order and label.
+#' @param ae_var A character string to define the variable of new column called ae
+#' @param ae_interested A character string including the interested AE structure, ex., AEDECOD, AESOC, AESER, AEREL, AEOUT
+#' @param stratum_var A character string to define the variable of baseline stratum in 'population_from'.Only one 'stratum_var' is allowed.
+#' @param listing_var  A character string to define the criteria to select the column of the table
+#'
+#' @return Return a standard adverse event data frame
+#' @export
+#' @examples
+#' db <- tidy_ae_table2(population_from  = adsl_mk1293,
+#'                     population_where = NULL,
+#'                     observation_from = adae_mk1293,
+#'                     observation_where = NULL,
+#'                     treatment_var = "ACTLARM",
+#'                     treatment_order = c("MK1293" = "MK-1293", "Placebo" = "Lantus"),
+#'                     ae_var = "AEDECOD",
+#'                     ae_interested = c("AESEV", "AEREL", "AEOUT"),
+#'                     listing_var = c("SITEID", "USUBJID", "RACE", "SEX", "AESER", "AEREL", "AEACN", "AEOUT",
+#'                                     "AESLIFE", "AESDTH", "AESOD", "AESMIE") )
+tidy_ae_table2 <- function(population_from,
+                           observation_from,
+                           population_where = "ITTFL=='Y'",
+                           observation_where = NULL,
+                           treatment_var = "TRTA",
+                           treatment_order = unique(population_from[[treatment_var]]),
+                           ae_var = ae_var,
+                           ae_interested = ae_interested,
+                           stratum_var = NULL,
+                           listing_var = names(observation_from)){
+
+  # Population Level Tidy Data
+  pop <- tidy_population1(population_from  = population_from,
+                          population_where = population_where,
+                          treatment_var    = treatment_var,
+                          treatment_order  = treatment_order,
+                          stratum_var      = stratum_var,
+                          baseline_var     = NULL)
+
+  # Select the Desired Observation
+  db <- tidy_observation(observation_from = observation_from,
+                         observation_where = observation_where,
+                         treatment_var    = treatment_var,
+                         treatment_order  = treatment_order)
+
+  # select the overlap pop(adsl) and db(adae)
+  db[["ae"]] <- db[[ae_var]]
+  db <- subset(db, USUBJID %in% pop$USUBJID)
+
+  # count the sample size of each arm
+  db_N <- dplyr::count(pop, treatment, stratum, name = "N")
+
+
+  res <- NULL
+  for (ae_idx in 1 : length(ae_interested)) {
+     res_new <- db %>% group_by(treatment, ae, eval(parse(text = paste0(ae_interested[ae_idx])))) %>%
+      summarise(n = n()) %>%
+      rename(ae_substructure = `eval(parse(text = paste0(ae_interested[ae_idx])))`) %>%
+      mutate(ae_structure = ae_interested[ae_idx]) %>%
+      left_join(db_N) %>%
+      mutate(pct = n / N * 100) %>%
+      ungroup() %>%
+      mutate(trtn = as.numeric(treatment)) %>%
+      select(- treatment) %>%
+      pivot_wider(names_from = trtn, values_from = c(n, N, pct), values_fill = 0) %>%
+      mutate(across(starts_with("N", ignore.case = FALSE), ~ max(.x)))
+     res <- rbind(res, res_new)
+  }
+
+  # res <- db %>% group_by(treatment, ae) %>%
+  #   summarise(n = n()) %>%
+  #   left_join(db_N) %>%
+  #   mutate(pct = n / N * 100) %>%
+  #   ungroup() %>%
+  #   mutate(trtn = as.numeric(treatment)) %>%
+  #   select(- treatment) %>%
+  #   pivot_wider(names_from = trtn, values_from = c(n, N, pct), values_fill = 0) %>%
+  #   mutate(across(starts_with("N", ignore.case = FALSE), ~ max(.x)))
+
+  listing_var <- unique(c("USUBJID", "ae", "treatment", listing_var))
+  list(table = res, listing = db[, listing_var])
+}
---
+++ R/tidy_population1.R
@@ -0,0 +1,74 @@
+#' tidy_population1
+#'
+#' This function is used to prepare the observation datasets for the use in function `tidy_ae_table()`.
+#' The function filter the `adsl` dataset, and define proper treatment order in the dataset.
+#'
+#' @param population_from A data frame to obtain population level variables.
+#'                        Typically an 'adsl' dataset.
+#'                        It is the source of variables mentioned in
+#'                        `population_where`, `treatment_var`, `stratum_var` and `covariate_var`.
+#' @param population_where A character string to define the criteria to select analysis population.
+#' @param treatment_var  A character string to define the variable of new column called "treatment".
+#' @param treatment_order A vector of character strings that tells the
+#'                function which rows of the table should be select,
+#'                only if the values in "treatment" is in this vector.
+#'                        It also provide the label names of the treatments after turning them into factors.
+#' @param stratum_var A character string to define the variable of baseline stratum in 'population_from'.
+#'                    Only one 'stratum_var' is allowed.
+#' @param baseline_var Tells the function which column of the table to be selected as the final output.
+#'
+#' @return an output data frame of population level information.
+#'         USUBJID: Unique subject identifier.
+#'         treatment: Actual treatment.
+#'         stratum: baseline stratum in 'population_from'.
+#'
+#' @export
+#'
+#' @examples
+#' pop <- tidy_population(population_from  = adsl %>% rename(TRTA = TRT01A),
+#'  population_where = "ITTFL=='Y'",
+#'  treatment_var    = "TRTA",
+#'  treatment_order  = c("MK9999" = "Xanomeline High Dose", "Placebo" = "Placebo"),
+#'  stratum_var      = NULL,
+#'  baseline_var     = NULL)
+#'
+#' tidy_population(population_from = adsl)
+#' tidy_population(population_from = adsl, stratum_var = "RACE")
+tidy_population1 <- function(population_from,
+                            population_where = NULL,
+                            treatment_var = "TRT01A",
+                            treatment_order = unique(population_from[[treatment_var]]),
+                            stratum_var = NULL,
+                            baseline_var = NULL){
+
+  ##################################
+  # End of Input Checking
+  ##################################
+
+  if(is.null(population_where)){ population_where <- TRUE}
+
+  # Select the Desired Population
+  pop <- eval(parse(text = paste0("subset(population_from,", population_where, ")")))
+
+  # Define treatment group
+  pop[["treatment"]] <- pop[[treatment_var]]
+  pop <- subset(pop, treatment %in% treatment_order)
+  
+  # Define treatment label
+  if (!is.null(names(treatment_order))) {
+    label_name = names(treatment_order)
+  } else {label_name = treatment_order}
+
+  pop[["treatment"]] <- factor(pop[["treatment"]], levels = treatment_order, labels = label_name)
+
+  # Define stratum
+  if(length(stratum_var) == 0){
+    pop[["stratum"]] <- "All"
+  }else{
+    pop[["stratum"]] <- pop[[stratum_var]]
+  }
+
+  pop <- pop[, unique(c("USUBJID", "treatment", "stratum", baseline_var))]
+
+  pop
+}
