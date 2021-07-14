c8ec3ba377cfbc5daacc45db4efed4579cb0dfbf
Melody J <melody@nbp-7-17.nbp.ruw.rutgers.edu>
2021-07-14 14:29:30 -0400

    draw axis test

--- tests/testthat/test-sparkline_errorbar.R
+++ tests/testthat/test-sparkline_errorbar.R
@@ -1,18 +1,23 @@
+library(testthat)
+library(htmlwidgets)
+library(plotly)
+devtools::load_all()
+
 plotly_snap <- function(p){
   p_js <- plotly::plotly_json(p, jsonedit = FALSE)
   p_lst <- jsonlite::fromJSON(p_js)
-  
+
   gsub(names(p_lst$visdat), "0000", as.character(p_lst))
-  
+
 }
 
-test_that("sparkline_deaw_axis",{
+test_that("sparkline_errorbar",{
   local_edition(3) # avoid Error: `expect_snapshot_output()` requires the 3rd edition.
-  color <- c("red", "blue")
-  label <- c("MK9999", "Placebo")
-  xlim  <- c(-2, 2)
-  xlab  <- "Example"
-  temp1<-sparkline_draw_axis(color, label, xlim,showlegend = FALSE, height = 30)
-  
+  est <- c(0,1,2)
+  lower <- c(-1, -2, -1)
+  upper <- c(3, 5, 5)
+  color <- c("red", "blue", "gold")
+  temp1<-sparkline_errorbar(est,lower,upper,color)
+
   expect_snapshot_output(plotly_snap(temp1))
 })
