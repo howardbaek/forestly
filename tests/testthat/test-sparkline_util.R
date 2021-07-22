240fde148ea59762e5fddd9abcbcabab4e4517d4
YitaoLiu1996 <liuyitao1996@gmail.com>
2021-07-22 16:26:27 -0400

    unit testing for sparkline_util

---
+++ tests/testthat/test-sparkline_util.R
@@ -0,0 +1,46 @@
+library(plotly)
+library(jsonlite)
+library(plotly)
+
+plotly_snap <- function(p){
+  p_js <- plotly_json(p, jsonedit = FALSE)
+  p_lst <- fromJSON(p_js)
+  gsub(names(p_lst$visdat), "0000", as.character(p_lst))
+}
+
+
+
+test_that("add_vline",{
+  
+  p1 <- plot_ly(cars, x = ~speed, y = ~dist)
+  temp1<-add_vline(p1, x = 7.5)
+  
+  
+  
+  expect_snapshot_output(plotly_snap(temp1))
+  
+})
+
+
+test_that("sparkline_legend",{
+  
+  p2 <- plot_ly(cars, x = ~speed, y = ~dist)
+  temp2<-sparkline_legend(p2, title = "cars plot")
+  
+  
+  
+  expect_snapshot_output(plotly_snap(temp2))
+  
+})
+
+
+test_that("sparkline_layout",{
+  
+  p3 <- plot_ly(cars, x = ~speed, y = ~dist)
+  temp3<-sparkline_layout(p3, color='red')
+  
+  
+  
+  expect_snapshot_output(plotly_snap(temp3))
+  
+})
