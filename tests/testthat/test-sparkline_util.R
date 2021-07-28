75efb2c4edf2977a7106a3144402d9fe75d66d7c
Yilong Zhang <yilong.zhang@merck.com>
2021-07-27 22:16:32 -0400

    update snapshot

--- tests/testthat/test-sparkline_util.R
+++ tests/testthat/test-sparkline_util.R
@@ -1,13 +1,10 @@
 library(plotly)
 library(jsonlite)
-library(plotly)
 
 test_that("add_vline",{
   
   p1 <- plot_ly(cars, x = ~speed, y = ~dist, type = "scatter", mode = 'markers')
   temp1<-add_vline(p1, x = 7.5)
-  
-  
   
   expect_snapshot_output(plotly_snap(temp1))
   
@@ -19,8 +16,6 @@ test_that("sparkline_legend",{
   p2 <- plot_ly(cars, x = ~speed, y = ~dist, type = "scatter", mode = 'markers')
   temp2<-sparkline_legend(p2, title = "cars plot")
   
-  
-  
   expect_snapshot_output(plotly_snap(temp2))
   
 })
@@ -30,8 +25,6 @@ test_that("sparkline_layout",{
   
   p3 <- plot_ly(cars, x = ~speed, y = ~dist, type = "scatter", mode = 'markers')
   temp3<-sparkline_layout(p3, color='red')
-  
-  
   
   expect_snapshot_output(plotly_snap(temp3))
   
