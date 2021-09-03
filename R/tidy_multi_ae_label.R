ac3a5f3c634139698a2d8fcc31459f997b6abed1
LittleBeannie <yujie.zhao@merck.com>
2021-09-02 21:41:29 -0400

    fix the test example in `tidy_multi_ae_label` related to `ae_interested`.

--- R/tidy_multi_ae_label.R
+++ R/tidy_multi_ae_label.R
@@ -28,8 +28,8 @@
 #'                                          labels = treatment_order), 
 #'                       N = c(3, 2))
 #' 
-#' tidy_multi_ae_label(db, db_N, ae_interested = ae_interested(ae_criterion = c('AESER == "Y"', 'AEREL != "N"'),
-#'                                                             ae_label = c("with serious adverse events","with drug-related adverse events")))
+#' tidy_multi_ae_label(db, db_N, ae_interested = define_ae_selectList(ae_criterion = c('AESER == "Y"', 'AEREL != "N"'),
+#'                                                                    ae_label = c("with serious adverse events","with drug-related adverse events")))
 
 tidy_multi_ae_label <- function(db, db_N, ae_interested){
   
