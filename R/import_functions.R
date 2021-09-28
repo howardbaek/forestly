f366ae58a0c0bafa0f231edb6fd1e27ac243d186
LittleBeannie <yujie.zhao@merck.com>
2021-09-28 10:11:46 -0400

    add `tidyr::complete()` into `import_functions.R`

--- R/import_functions.R
+++ R/import_functions.R
@@ -1,4 +1,5 @@
 #' @importFrom plotly plot_ly
-#' @importFrom dplyr count group_by summarise n left_join mutate ungroup select across starts_with filter
+#' @importFrom dplyr count group_by summarise n left_join mutate ungroup select across starts_with filter rename
 #' @importFrom tidyr pivot_wider
+#' @importFrom tidyr complete
 NULL
