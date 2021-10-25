c12f41322c7380944af8d2096eca4a8868c8dc32
LittleBeannie <yujie.zhao@merck.com>
2021-10-25 08:40:45 -0400

    edit tlf_download

--- R/tlf_download.R
+++ R/tlf_download.R
@@ -34,7 +34,7 @@ tlf_download <- function(path, output_name = "AE TLF", button_label = "Download 
   
   if(is.null(path)) stop("Please give the path to download files!")
   
-  download_file(
+  downloadthis::download_file(
     path = list.files(path = path, full.names = TRUE),
     output_name = output_name,
     button_label = button_label,
