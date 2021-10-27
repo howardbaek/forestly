6528c21a11269d7fed23953930fe015b41172ecb
Wang, Benjamin <benjamin.wang@merck.com>
2021-10-27 11:13:12 -0400

    test cases for define_ae_select_list()

--- tests/testthat/test-define_ae_select_list.R
+++ tests/testthat/test-define_ae_select_list.R
@@ -1,7 +1,19 @@
 test_that("define_ae_select_list() gives errors if the number of ae_label does't match that of ae_criterion", {
-  expect_equal(2 * 2, 4)
+  expect_error(define_ae_select_list(ae_criterion = c('AESER != "N"', 'AEREL != "None"'),
+                                     ae_label = c("with serious adverse events")))
+})
+
+test_that("missing argument would generate errors", {
+  expect_error(define_ae_select_list(ae_criterion = c('AESER != "N"', 'AEREL != "None"')))
+  expect_error(define_ae_select_list(ae_label = c("with serious adverse events", 
+                                                  "with drug-related adverse events")))
 })
 
 test_that("compare with the output from list()", {
-  expect_equal(2 * 2, 4)
+  expect_equal(define_ae_select_list(ae_criterion = c('AESER != "N"', 'AEREL != "None"'),
+                                     ae_label = c("with serious adverse events", 
+                                                  "with drug-related adverse events")), 
+               list(interested_ae_criterion = c('AESER != "N"', 'AEREL != "None"'),
+                    interested_ae_label =  c("with serious adverse events", 
+                                             "with drug-related adverse events")))
 })
